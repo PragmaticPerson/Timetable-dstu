c54bdafce2d28663c1761a9d3e0ae6fdde1fa2f4
Alex Chereshnev <aleksejceresnev43@yandex.ru>
2022-10-18 18:13:02 +0300

    Update URL endpoints

--- src/main/java/ru/donstu/edu/controllers/AdminRestController.java
+++ src/main/java/ru/donstu/edu/controllers/AdminRestController.java
@@ -4,45 +4,41 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import ru.donstu.edu.dao.GroupJpaRepository;
-import ru.donstu.edu.models.Group;
 import ru.donstu.edu.models.TimetableResponce;
 import ru.donstu.edu.service.TimetableService;
-import ru.donstu.edu.service.UpdateDatabaseService;
+import ru.donstu.edu.service.UpdateDatabaseScheduler;
 
 @RestController
 public class AdminRestController {
-    private UpdateDatabaseService updateService;
-    private GroupJpaRepository groupRepository;
+    private UpdateDatabaseScheduler updateService;
     private TimetableService timetableService;
 
     @Autowired
-    public AdminRestController(UpdateDatabaseService updateService, GroupJpaRepository groupRepository,
-            TimetableService timetableService) {
+    public AdminRestController(UpdateDatabaseScheduler updateService, TimetableService timetableService) {
         super();
         this.updateService = updateService;
-        this.groupRepository = groupRepository;
         this.timetableService = timetableService;
     }
 
-    @GetMapping(path = "/groups/update")
-    public List<Group> updateGroups() throws IOException {
-        updateService.updateGroups();
-        return groupRepository.findAll();
-    }
-
-    @GetMapping(path = "/timetable/{group}/update")
-    public List<TimetableResponce> updateTimetable(@PathVariable("group") int id) throws IOException {
-        updateService.updateTimetable(id);
-        return timetableService.getTimetableForGroup(id);
+    @GetMapping(path = "/admin/update")
+    @ResponseStatus(code = HttpStatus.OK)
+    public void updateAllTimetable() throws IOException {
+        updateService.updateInformation();
     }
 
     @GetMapping(path = "/timetable/{group}")
     public List<TimetableResponce> getTimetable(@PathVariable("group") int id) {
         return timetableService.getTimetableForGroup(id);
+    }
+
+    @GetMapping(path = "/timetable/audience/{audience}")
+    public List<TimetableResponce> getTimetableGForAudience(@PathVariable("audience") int id) {
+        return timetableService.getTimetableForAudience(id);
     }
 }
