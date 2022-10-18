23a67c89606a134aa7dae1b1e52ab9d0c52949cf
Alex Chereshnev <aleksejceresnev43@yandex.ru>
2022-10-18 18:31:59 +0300

    Rename controller. Add endpoint to get all groups

--- src/main/java/ru/donstu/edu/controllers/MainRestController.java
+++ src/main/java/ru/donstu/edu/controllers/MainRestController.java
@@ -10,19 +10,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import ru.donstu.edu.models.Group;
 import ru.donstu.edu.models.TimetableResponce;
+import ru.donstu.edu.service.GroupService;
 import ru.donstu.edu.service.TimetableService;
 import ru.donstu.edu.service.UpdateDatabaseScheduler;
 
 @RestController
-public class AdminRestController {
+public class MainRestController {
+
     private UpdateDatabaseScheduler updateService;
+    private GroupService groupService;
     private TimetableService timetableService;
 
     @Autowired
-    public AdminRestController(UpdateDatabaseScheduler updateService, TimetableService timetableService) {
+    public MainRestController(UpdateDatabaseScheduler updateService, GroupService groupService,
+            TimetableService timetableService) {
         super();
         this.updateService = updateService;
+        this.groupService = groupService;
         this.timetableService = timetableService;
     }
 
@@ -30,6 +36,11 @@ public class AdminRestController {
     @ResponseStatus(code = HttpStatus.OK)
     public void updateAllTimetable() throws IOException {
         updateService.updateInformation();
+    }
+
+    @GetMapping(path = "/groups")
+    public List<Group> getAllGroups() {
+        return groupService.findAll();
     }
 
     @GetMapping(path = "/timetable/{group}")
